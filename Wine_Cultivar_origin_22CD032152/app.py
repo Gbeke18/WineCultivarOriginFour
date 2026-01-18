49c5d74bb922910243c81740889f396d190958dd
Gbeke18 <gbekeiyun@gmail.com>
2026-01-18 14:14:41 +0100

    Add files via upload

---
+++ Wine_Cultivar_origin_22CD032152/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import numpy as np
+import joblib
+
+app = Flask(__name__)
+
+# Load model and scaler
+model = joblib.load(r"C:\Users\HP\Downloads\Wine_Cultivar_origin_22CD032152\model\wine_cultivar_model.pkl")
+scaler = joblib.load(r"C:\Users\HP\Downloads\Wine_Cultivar_origin_22CD032152\model\scaler.pkl")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    prediction = None
+
+    if request.method == "POST":
+        features = [
+            float(request.form["alcohol"]),
+            float(request.form["malic_acid"]),
+            float(request.form["alcalinity_of_ash"]),
+            float(request.form["magnesium"]),
+            float(request.form["color_intensity"]),
+            float(request.form["proline"])
+        ]
+
+        scaled_features = scaler.transform([features])
+        pred = model.predict(scaled_features)[0]
+
+        prediction = f"Cultivar {pred + 1}"
+
+    return render_template("index.html", prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
